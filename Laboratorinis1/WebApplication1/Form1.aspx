1556d6fe6b0df47b4095dfeabe3867571cd3a31e
Nedas1123 <61797172+Nedas1123@users.noreply.github.com>
2021-04-19 22:25:03 +0300

    Add files via upload

---
+++ Laboratorinis1/WebApplication1/Form1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form1.aspx.cs" Inherits="WebApplication1.Form1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Pradiniai duomenys:"></asp:Label>
+            <br />
+            <asp:Label ID="Label2" runat="server"></asp:Label>
+            <asp:Label ID="Label3" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Laboratorinis1/WebApplication1/Form1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebApplication1
+{
+    public partial class Form1 : System.Web.UI.Page
+    { 
+        string Data ="App_data/U3.txt";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ReadData(Data);
+            LoadData();
+        }
+
+        private void ReadData(string data)
+        {
+            using (StreamReader reader=File.Rea
+        }
+
+        private void LoadData()
+        {
+
+
+        }
+    }
+}
